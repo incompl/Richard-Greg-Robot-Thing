b876312e75f018544e96f2e45e86572eacb77832
Richard D. Worth <rdworth@gmail.com>
2012-03-29 16:16:21 -0400

    Added wall drawing

--- canvas.js
+++ canvas.js
@@ -1,5 +1,5 @@
 (function() {
-
+	
 	window.rr.draw = function(board, canvas) {
 		var row, col,
 			size = window.rr.size,
@@ -13,17 +13,37 @@
 				
 				var y = (row + 0.5) * scale;
 				
+				// Draw tile floor
 				ctx.fillStyle = "silver";
 				ctx.strokeStyle = "rgb(220, 220, 220)";
 				ctx.fillRect(x + scale * .1, y + scale * .1, scale * .8, scale * .8);
 				ctx.strokeRect(x, y, scale, scale);
 				
+				// Draw robots
 				if (tile.robot) {
 					ctx.beginPath();
 					ctx.fillStyle = tile.robot;
 					ctx.arc(x + scale / 2, y + scale / 2, scale / 3, 0, Math.PI*2, true);
 					ctx.closePath();
 					ctx.fill();
+				}
+				
+				// Draw walls
+				if (tile.northWall) {
+					ctx.fillStyle = "gray";
+					ctx.fillRect(x, y, scale, scale * .1);
+				}
+				if (tile.southWall) {
+					ctx.fillStyle = "gray";
+					ctx.fillRect(x, y + scale * .9, scale, scale * .1);
+				}
+				if (tile.eastWall) {
+					ctx.fillStyle = "gray";
+					ctx.fillRect(x + scale * .9, y, scale * .1, scale);
+				}
+				if (tile.westWall) {
+					ctx.fillStyle = "gray";
+					ctx.fillRect(x, y, scale * .1, scale);
 				}
 				
 			}
