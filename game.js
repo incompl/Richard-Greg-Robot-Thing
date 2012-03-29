28e21838c095d54548f10bf5e6db4436c9ba19e9
Greg <greg@bocoup.com>
2012-03-29 15:22:05 -0400

    data structure comment

--- game.js
+++ game.js
@@ -1,3 +1,29 @@
+/*
+
+All possible cell properties:
+
+ * southWall   true
+ * northWall   true
+ * eastWall    true
+ * westWall    true
+ * robot       "red", "green", "yellow", "blue"
+ * target      {color: "red", shape: "moon"}
+
+Shapes (for targets)
+
+ * moon
+ * planet
+ * star
+ * triangle
+ * black hole
+
+*/
+
+window.board = [
+  [{southWall:true}, {}, {}],
+  [{norhtWall:true}, {}, {}]
+];
+
 (function() {
 
     alert("HI");
